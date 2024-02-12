d3e28e9c26d3e49c698795cc3bedb24b85657cba
Mr-Jitesh9170 <mr.jiteshpandey9170@gmail.com>
2024-02-12 23:37:53 +0530

    make add and substract button as functional

--- src/DrawingApp/drawing.jsx
+++ src/DrawingApp/drawing.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import "./drawing.scss";
 
 export const DrawingApp = () => {
+  const [control, setControl] = useState(0)
+  const handleChange = (e) => {
+    let value = e.target.innerHTML;
+    if (value === "-") {
+      setControl(control - 1)
+    } else if (value === "+") {
+      setControl(control + 1)
+    }
+
+  }
   return (
     <div className="drawing-container">
       <div className="drawing-top"> </div>
       <div className="drawing-bottom">
         <div className="part1">
-          <div className="box">-</div>
-          <div className="box">10</div>
-          <div className="box">+</div>
+          <div className="box" onClick={handleChange}>-</div>
+          <div className="box">{control}</div>
+          <div className="box" name="increase" onClick={handleChange}>+</div>
           <div className="box"></div>
         </div>
         <div className="box">X</div>
